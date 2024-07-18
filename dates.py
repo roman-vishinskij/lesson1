347122607318629a0d6c6034269b053fc0ee679e
roman-vishinskij <105998414+roman-vishinskij@users.noreply.github.com>
2024-07-18 07:11:29 +0300

    Add files via upload

---
+++ dates.py
@@ -0,0 +1,24 @@
+print(5//5) # // - целочисленное деление /-деление
+print(type(5)) # int-Integrator - целые числа
+print(5%5) # %-остаток от деление
+print(5**5) # - возведение в степень
+print(2.0)
+print(type(2.0)) #-Float -тип числа челые и дробные
+print('Hello, World!') # - '' ""  ввод текста в ковычках
+print(type('Hello, World!')) # string -строка - тип данных
+print('Hello,'+'World') # concatenate - сложение
+print('1' + '1')
+print(1+1)
+print(True, False) # boolean - логическое значение
+print(type(True),type(False)) # bool clsass
+print(5 > 10, 10 > 5)
+print(1, 2, 3, 4, 'hello world', True) # - можно выводить множество значений в одной строке
+print( 5 <= 5, 5 == 5, 5 != 5) #  == -равно, !=- не равно
+print( 5 != 5  and 5 < 10  ) # and - и  , or - или в ответе False -
+print( 5 == 5 and 5 < 10) # True  в ответе
+print(5 == 5 or 5 < 10) # True  в ответе
+print( 5 != 5  or 5 < 10 ) # True  в ответе
+print( type(int('5'))) # перевести  в другой класс
+print( type(str('5')))
+print( type(float('5')))
+print( type(bool('5')))
